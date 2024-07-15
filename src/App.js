3338ae8de1b42def6cd9eca9634056a13f1a1b91
kshitijbahul <chunks.3@gmail.com>
2024-07-15 16:05:20 +0200

    chore: compressed code to use repeatable elements

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: '',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: '',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: '',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: '',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: '',
+    },
+  ];
+
   return (
     <div className="categories-container">
-        <div className="category-container">
-          <img src="https://via.placeholder.com/150" alt="category" />
-          <div className="category-body-contaner">
-            <h2>Hats</h2>
-            <p>Show Now</p>
+      { categories.map((category) => 
+         (
+          <div className="category-container" key={category.id}>
+            <div className="background-image">
+              <img src="https://via.placeholder.com/150" alt="category" />
+            </div>
+            <div className="category-body-contaner">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
           </div>
-        </div>
-        <div className="category-container">
-          <img src="https://via.placeholder.com/150" alt="category" />
-          <div className="category-body-contaner">
-            <h2>Jackets</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <img src="https://via.placeholder.com/150" alt="category" />
-          <div className="category-body-contaner">
-            <h2>Sneakers</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <img src="https://via.placeholder.com/150" alt="category" />
-          <div className="category-body-contaner">
-            <h2>Womens</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <img src="https://via.placeholder.com/150" alt="category" />
-          <div className="category-body-contaner">
-            <h2>Mens</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
+        )
+      ) 
+    }
     </div>
   );
 }
