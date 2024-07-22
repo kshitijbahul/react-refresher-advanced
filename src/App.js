458f73d264b7f763addd7e65f1e8b4b9c3f845c6
kshitijbahul <chunks.3@gmail.com>
2024-07-22 14:02:50 +0200

    chore: Added Basic Nav Menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
-import Home from './routes/home/home.components';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
 
  
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element = {<Home></Home>} />
+      <Route path='/' element = {<Navigation />}>
+        <Route index={true} element = {<Home />} /> 
+      </Route>
+      
     </Routes>
     
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as SiteLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <SiteLogo className="logo"/>
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+              SHOP
+            </Link>
+            <Link className="nav-link" to="/contact">
+              CONTACT
+            </Link>
+            <Link className="nav-link" to="/home">
+              SIGN IN
+            </Link>
+          </div>
+        </div>
+        <Outlet/>
+      </Fragment>
+    );
+}
+
+export default Navigation;
