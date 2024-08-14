ca3bed87733714fb8e41a6f076bbe61a00a6b968
kshitijbahul <chunks.3@gmail.com>
2024-08-14 15:24:19 +0200

    error: committed to remeber the wrong way of creating a reducer

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     const existingItem = cartItems.find(
@@ -14,6 +15,70 @@ const addCartItem = (cartItems, productToAdd) => {
     
 }
 
+export const CART_ACTION_TYPES = {
+    ADD_ITEM_TO_CART : 'ADD_ITEM_TO_CART',
+    REMOVE_ITEM_FROM_CART : 'REMOVE_ITEM_FROM_CART',
+    DECREASE_ITEM_COUNT_FROM_CART : 'DECREASE_ITEM_COUNT_FROM_CART',
+    TOGGLE_CART : 'TOGGLE_CART',
+}
+const removeItemFromCart = (cartItems,removedProduct) => {
+    return cartItems.filter(( cardItem )=> cardItem.id !== removedProduct.id);
+}
+const decreaseItemFromCart = (cartItems,removedProduct) =>  {
+    const cartElement = cartItems.find((cartItem) => cartItem.id === removedProduct.id );
+    if (cartElement.quantity === 1) {
+        return removeItemFromCart(removedProduct);
+    } else {
+        const newCartItems = cartItems.map((cartItem) => 
+            cartItem.id === removedProduct.id ? {...cartItem,quantity: cartItem.quantity-1} : cartItem
+        );
+        return newCartItems;
+    }
+    }
+    
+const INITIAL_STATE = {
+    cartOpened: false,
+    cartItems: [],
+    cartValue: 0,
+    cartSize: 0
+}
+
+export const cartReducer = (state, action) => {
+    
+    const { cartItems, cartOpened,cartSize } = state;
+    const { type, payload } = action;
+    switch(type) {
+        case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
+            return {
+                ...state,
+                cartItems: addCartItem(cartItems, payload),
+                cartSize: cartSize + 1,
+                cartCost: cartCost + payload.price
+
+            }
+        case CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(cartItems, payload),
+                cartSize: cartSize 
+            }
+        case CART_ACTION_TYPES.DECREASE_ITEM_COUNT_FROM_CART:
+            return {
+                ...state,
+                cartItems: decreaseItemFromCart(cartItems, payload),
+                cartSize: cartSize - 1,
+            }
+        case CART_ACTION_TYPES.TOGGLE_CART:
+            return {
+                ...state,
+                cartOpened: !cartOpened
+            }
+        default:
+            throw new Error(`Unknown action type: ${type} in cartReducer`);
+
+    }
+
+}
 
 
 export const CartContext = createContext({
@@ -28,10 +93,12 @@ export const CartContext = createContext({
 });
 
 export const CartProvider = ({children}) => {
-    const [ cartItems, setCartItems] = useState([]);
+    /* const [ cartItems, setCartItems] = useState([]);
     const [ cartOpened, setCartOpened ] = useState(false);
     const [ cartSize, setCartSize ] = useState(0);
-    const [ cartValue, setCartValue ] = useState(0);
+    const [ cartValue, setCartValue ] = useState(0); */
+    const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE);
+    const {cartItems, cartOpened, cartSize, } = state;
     useEffect(() => {
         const cartItemCount = cartItems.reduce((cartCount,eachCartItem) => cartCount+eachCartItem.quantity ,0);
         setCartSize(cartItemCount);
@@ -43,28 +110,42 @@ export const CartProvider = ({children}) => {
     },[cartItems]);
     
     const addItemToCart = (productToAdd) => {
-        const newCartItems = addCartItem(cartItems, productToAdd);
-        setCartItems(newCartItems);
+        /* const newCartItems = addCartItem(cartItems, productToAdd);
+        setCartItems(newCartItems); */
+        dispatch(
+            {
+                type: CART_ACTION_TYPES.ADD_ITEM_TO_CART.replace,
+                payload: productToAdd
+            }
+        );
     }
     const removeItemFromCart = (removedProduct) => {
-        const removedItems = cartItems.filter((cartItem)=> cartItem.id !== removedProduct.id);
-        setCartItems(removedItems);
+        dispatch({
+            type: CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART,
+            payload: removedProduct
+        });
     }
     const decreaseItemFromCart = (removedProduct) =>  {
-        const cartElement = cartItems.find((cartItem) => cartItem.id === removedProduct.id );
-        if (cartElement.quantity === 1) {
-            removeItemFromCart(removedProduct);
-        } else {
-            const newCartItems = cartItems.map((cartItem) => 
-                cartItem.id === removedProduct.id ? {...cartItem,quantity: cartItem.quantity-1} : cartItem
-            );
-            setCartItems(newCartItems);
-        }
+        dispatch({
+            type: CART_ACTION_TYPES.DECREASE_ITEM_COUNT_FROM_CART,
+            payload: removedProduct
+        });
     }
     const toggleCart = () => {
-        setCartOpened(!cartOpened);
+        dispatch({
+            type: CART_ACTION_TYPES.TOGGLE_CART
+        });
     }
-    const providerValue = { cartOpened,toggleCart, cartItems, addItemToCart, cartSize, decreaseItemFromCart, removeItemFromCart, cartValue };
+    const providerValue = { 
+        cartOpened,
+        toggleCart,
+        cartItems,
+        addItemToCart,
+        cartSize,
+        decreaseItemFromCart,
+        removeItemFromCart,
+        cartValue 
+    };
     
     return (
         <CartContext.Provider value={providerValue}> 
