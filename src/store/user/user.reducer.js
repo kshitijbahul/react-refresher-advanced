9a8c1403ad7438553b95cf586ed1b235ae2a60ea
kshitijbahul <chunks.3@gmail.com>
2024-08-15 15:22:36 +0200

    feat : Added a User Reducer

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,46 @@
+
+/// This is same as what we had in context but some changes 
+
+
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+
+
+
+/* 
+    CHANGE 1: 
+    We do this because now we don't use the useReducer hook, 
+    so there is nothing to set the initial value of the state
+    Hence we now pass the initial value of the state as the default value
+*/
+//const userReducer = (state , action) => {
+export const userReducer = (state = INITIAl_STATE , action) => {
+    console.log('Dispatched action');
+    console.log('state', state);
+    
+    console.log('action', action);
+    const { type, payload } = action;
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state, // Get everything in the state , and only update the values that you need to update
+                currentUser: payload,
+            };
+        default:
+            /* 
+                CHANGE 2: 
+                Since each reducer will get all the actions fired, 
+                it is important that we return the state as it is from the reducer to indicate to react 
+                that nothing has changed and hence 
+                control the re-rendering of the component
+            */
+            //throw new Error(`Unknown action type: ${type} in userReducer`);
+            return state;
+    }
+    
+}
+const INITIAl_STATE = {
+    currentUser: null,
+}
