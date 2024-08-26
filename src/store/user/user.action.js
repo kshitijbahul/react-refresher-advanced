79c047f002eb221488799a69992f0a7e1cca1e79
kshitijbahul <chunks.3@gmail.com>
2024-08-26 13:47:57 +0200

    chore: cleaned up

--- src/store/user/user.action.js
+++
@@ -1,8 +0,0 @@
-import { USER_ACTION_TYPES } from "./user.types";
-export const setCurrentUser = (user) => {
-    // Return an Action Object 
-    return {
-        type: USER_ACTION_TYPES.SET_CURRENT_USER,
-        payload: user,
-    };
-}
