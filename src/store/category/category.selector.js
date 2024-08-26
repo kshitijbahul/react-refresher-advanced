13c66d5bcd06fe1cd5865a26d7a58c0adb4451dc
kshitijbahul <chunks.3@gmail.com>
2024-08-26 09:51:45 +0200

    feat: introduced reselect

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,3 +1,22 @@
+import { createSelector } from "reselect";
+
+
+const selectCategoryReducer = (state) => state.categories; 
+
+const selectCategories = (state) => createSelector(
+    [selectCategoryReducer], // This is an input selector
+    (categoriesSlice) => categoriesSlice.categories // This is the output selector i.e when the categories has not chnaged on the Root state, it will return the same value
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((acc, category)=> {
+        const {title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+);
+/* 
 export const selectCategoriesMap = (state) => {
     console.log('Category Selector fired, ', state);
     return state.categories.categories.reduce((acc, category)=> {
@@ -6,4 +25,4 @@ export const selectCategoriesMap = (state) => {
         return acc;
     }, {});
     
-} // This is a selector function
+} // This is a selector function */
