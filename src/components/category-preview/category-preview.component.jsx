dd3faea129d305f55548110604a894011d381ef2
kshitijbahul <chunks.3@gmail.com>
2024-08-27 12:17:07 +0200

    feat: Introduced a spinner

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
 
 import ProductCard from "../../components/product-card/product-card.component";
 import {CategoryPreviewContainer, Title, Preview} from './category-preview.styles.jsx';
+import {selectCategoriesIsLoading} from '../../store/category/category.selector.js'
+
+import Spinner from '../spinner/spinner.component';
+
 
 const CategoryPreview = ({title, products}) => {
+    const isLoading = useSelector(selectCategoriesIsLoading);
     return (
+        isLoading ? <Spinner/> :
         <CategoryPreviewContainer>
             <Title to={`${title}`}>
                 {title.toUpperCase()}
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import { selectCategoriesMap } from '../../store/category/category.selector';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/category/category.selector';
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component'
 
 import {CategoryContainer, Title } from './category.styles.jsx';
 
@@ -11,6 +12,7 @@ const Category = () => {
     const { category } = useParams(); 
     console.log('Rendering category component');
     const categoriesMap  = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [ products, setProducts ] = useState(categoriesMap[category]);
     useEffect(()=> {
         console.log('Setting product');
@@ -21,16 +23,21 @@ const Category = () => {
             <Title>
                     {category.toUpperCase()}
             </Title>
-            <CategoryContainer>
-                
-                {   
-                    // Added a check for products since products is loaded async and on the first render it can be empty
-                    // this is a good practice to avoid errors
-                    products && products.map(
-                        (categoryItem) => (<ProductCard key={categoryItem.id} product = {categoryItem} />)
-                    )
-                }
-            </CategoryContainer>
+            {
+                isLoading ? 
+                    <Spinner/> 
+                    : 
+                    <CategoryContainer>
+                    {   
+                        // Added a check for products since products is loaded async and on the first render it can be empty
+                        // this is a good practice to avoid errors
+                        products && products.map(
+                            (categoryItem) => (<ProductCard key={categoryItem.id} product = {categoryItem} />)
+                        )
+                    }
+                    </CategoryContainer>
+            }
+            
         </Fragment>
     );
 }
